174367bd14c73778e3e43f62cc78c00f51596493
ncaung <forebus.pcone@gmail.com>
2017-10-22 20:33:07 +0630

    (Fixed) error

--- SendingDataBetweenActivities/app/src/main/java/com/zakar/ncaung/sendingdatabetweenactivities/activity/SecondActivity.java
+++ SendingDataBetweenActivities/app/src/main/java/com/zakar/ncaung/sendingdatabetweenactivities/activity/SecondActivity.java
@@ -10,11 +10,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
+import com.zakar.ncaung.sendingdatabetweenactivities.MainApplication;
 import com.zakar.ncaung.sendingdatabetweenactivities.Parcel.User;
 import com.zakar.ncaung.sendingdatabetweenactivities.R;
 
 public class SecondActivity extends AppCompatActivity {
-
+    EditText edtAge;
+    EditText edtAddress;
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +25,15 @@ public class SecondActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        EditText edtAge = (EditText)findViewById(R.id.edtAge);
-        EditText edtAddress =(EditText)findViewById(R.id.edtAddress);
+         user = MainApplication.getUser();
+         edtAge = (EditText)findViewById(R.id.edtAge);
+         edtAddress =(EditText)findViewById(R.id.edtAddress);
 
-        Bundle args = getIntent().getExtras();
-        User user = args.getParcelable("user");
-
-        Log.d("USER_NAME",String.valueOf(user.getUser_name()));
-        Log.d("USER_EMAIL",String.valueOf(user.getUser_email()));
+//        Bundle args = getIntent().getExtras();
+//         user = args.getParcelable("user");
+//
+//        Log.d("USER_NAME",String.valueOf(user.getUser_name()));
+//        Log.d("USER_EMAIL",String.valueOf(user.getUser_email()));
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -37,15 +41,15 @@ public class SecondActivity extends AppCompatActivity {
             public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-//                user.setUser_age(edtAge.getText().toString());
-//                user.setUser_address(edtAddress.getText().toString());
-//
-//                Bundle args = new Bundle();
-//                args.putParcelable("user",user);
+                user.setUser_age(edtAge.getText().toString());
+                user.setUser_address(edtAddress.getText().toString());
+
+                Bundle args = new Bundle();
+                args.putParcelable("user",user);
 
 
                 Intent intent = new Intent(SecondActivity.this,ThirdActivity.class);
-//                intent.putExtras(args);
+                intent.putExtras(args);
                 startActivity(intent);
             }
         });
@@ -54,14 +58,15 @@ public class SecondActivity extends AppCompatActivity {
 
 
 //    @Override
-//    protected void onResume() {
-//        super.onResume();
-//        if(user.getUser_age() != null){
-//            edtAge.setText(user.getUser_age());
-//            edtAge.setSelection(user.getUser_age().length());
-//        }
-//        if(user.getUser_address() != null){
-//            edtAddress.setText(user.getUser_address());
-//        }
-//    }
+    protected void onResume() {
+        super.onResume();
+        if(user.getUser_age() != null){
+            edtAge.setText(user.getUser_age());
+            edtAge.setSelection(user.getUser_age().length());
+        }
+        if(user.getUser_address() != null){
+            edtAddress.setText(user.getUser_address());
+            edtAddress.setSelection(user.getUser_address().length());
+        }
+    }
 }
--- SendingDataBetweenActivities/app/src/main/java/com/zakar/ncaung/sendingdatabetweenactivities/activity/ThirdActivity.java
+++ SendingDataBetweenActivities/app/src/main/java/com/zakar/ncaung/sendingdatabetweenactivities/activity/ThirdActivity.java
@@ -7,16 +7,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import com.zakar.ncaung.sendingdatabetweenactivities.Parcel.User;
 import com.zakar.ncaung.sendingdatabetweenactivities.R;
 
 public class ThirdActivity extends AppCompatActivity {
-
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        Bundle args = getIntent().getExtras();
+        user = args.getParcelable("user");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
